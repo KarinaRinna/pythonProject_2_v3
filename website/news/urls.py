dbc03f3fe7579357b0d63f8e1e5f355a1638e2c9
Mandarin <zurual@mail.ru>
2023-02-26 18:52:38 +0300

    v_4

--- website/news/urls.py
+++ website/news/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'), # тут отслеживаем url 'create' в /news (10 строка в website/urls.py)
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail'),
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name='news-update'),
+    path('<int:pk>/delete', views.NewsDeleteView.as_view(), name='news-delete')
 ]
--- website/news/views.py
+++ website/news/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render, redirect
 from .models import Artiles
 from .forms import ArtilesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
+
 
 def news_home(request):
     news = Artiles.objects.order_by('-date') #Сортируем по дате публикации
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Artiles
+    template_name = 'news/details_view.html'
+    context_object_name = 'artile'
+
+
+class NewsUpdateView(UpdateView):
+    model = Artiles
+    template_name = 'news/create.html'
+
+    form_class = ArtilesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Artiles
+    success_url = '/news/'          #тут после удаления переадресовываем на невс
+    template_name = 'news/news-delete.html'
+
 
 def create(request):
     error = ''
